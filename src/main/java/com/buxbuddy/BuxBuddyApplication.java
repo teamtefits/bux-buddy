de341080936c9e56d2a4a7ac5b6678b34be81726
naveennarayantech <naveen.narayan.tech@gmail.com>
2026-06-25 15:45:48 -0600

    Initial commit - Bux Buddy

---
+++ src/main/java/com/buxbuddy/BuxBuddyApplication.java
@@ -0,0 +1,13 @@
+package com.buxbuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuxBuddyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuxBuddyApplication.class, args);
+    }
+
+}
